87d77b695a4041d72cfd22531fdc9c314b710c8b
Rui Han <iurnah@gmail.com>
2014-04-24 22:49:25 -0400

    add the first version of MemoryAnalyzer.cpp.h

--- qemu/s2e/Plugins/MemoryAnalyzer/MemoryAnalyzer.cpp
+++ qemu/s2e/Plugins/MemoryAnalyzer/MemoryAnalyzer.cpp
@@ -20,20 +20,139 @@ namespace Plugins {
 
 S2E_DEFINE_PLUGIN(MemoryAnalyzer, "Memory Analyzer Plugin", "MemoryAnalyzer", "ExecutionTracer");
 
+MemoryAnalyzer::MemoryAnalyzer(S2E* s2e): Plugin(s2e) {}
+
 /*********************************************************************************
- *	initialize the shadown memory
+ *	initialize the MemoryAnalyzer
+ *	(1) connect to the dependency plugins
+ *	(2) reading the configured values
  *********************************************************************************/
 void MemoryAnalyzer::initialize(){
+	m_tracer = static_cast<ExecutionTracer*>(s2e()->getPlugin("ExecutionTracer"));
+	m_executionDetector = static_cast<ModuleExecutionDetector*>(s2e()->getPlugin("ModuleExecutionDetector"));
 
+	m_monitorModules = s2e()->getConfig()->getBool(getConfigKey() + ".monitorModules");
+	if (m_monitorModules && !m_execDetector) {
+		s2e()->getWarningsStream() << "MemoryAnalyzer: The monitorModule option requires ModuleExecutionDetector\n";
+		exit(-1);
+	}
+
+	m_memoryMonitor = s2e()->getConfig()->getBool(getConfigKey() + ".memoryMonitor");
+	m_stackMonitor = s2e()->getConfig()->getBool(getConfigKey() + ".stackMonitor");
+	m_heapMonitor = s2e()-getConfig()->getBool(getConfigKey() + ".heapMonitor");
+
+	s2e()->getDebugStream() << "MonitorMemory: " << m_monitorMemory << "StackMonitor" 
+			<< m_stackMonitor << "HeapMonitor" << m_heapMonitor << std::endl;
+
+	enableTracing();
+
+	/* Initialize the shadow memory */
 	init_shadow_memory();	
-
 }
 
 /*********************************************************************************
- *	update the memory shadow based on the loaded module.	
+ *	
  *********************************************************************************/
-void onModuleLoad(){
+void MemoryAnalyzer::enableTracing(){
+	if(m_monitorMemory){
+		s2e()->getMessagesStream() << "MemoryAnalyzer Plugin: Enable memory tracing" << std::endl;
+		m_DataMemoryMonitor.disconnect();
+		
+		if(m_monitorModules){
+			//XXX: is this saying that we are going to change to another module
+			m_executionDetector->onModuleTransition.connect(	//handle the module detection
+					sigc::mem_fun(*this, &MemoryAnalyzer::onModuleTransition));
+		} else {
+			m_DatamemoryMonitor = s2e()->getCorePlugin()->onDataMemoryAccess.connect(
+					sigc::mem_fun(*this, &MemoryAnalyzer::onDataMemoryAccess));
+		}
+	}
+}
 
+/*********************************************************************************
+ *	
+ *********************************************************************************/
+void MemoryAnalyzer::onModuleTransition(S2EExecutionState *state,
+										const ModuleDescriptor *prevModule,
+										const ModuleDescriptor *nextModule)
+{
+	ModuleDescriptor *currentDescriptor = m_executionDetector->getCurrentDescriptor(state);
+
+	if(currentDescriptor->name == nextModule->name && !m_DataMomoryMonitor.connect()){
+		m_DataMemoryMonitor = s2e()->getCorePlugin()->onDataMemoryAccess.connect(
+				sigc::mem_fun(*this, &MemoryAnalyzer::onDataMemoryAccess));		
+	} else {
+		m_DataMemoryMonitor.disconnect();
+	}
+}
+
+/*********************************************************************************
+ *	
+ *********************************************************************************/
+void MemoryAnalyzer::onDataMemoryAccess(S2EExecutionState *state,
+								klee::ref<klee::Expr> address,
+								klee::ref<klee::Expr> hostAddress,
+								klee::ref<klee::Expr> value,
+								bool isWrite, bool isIO)
+{	//XXX:there is a hack. use with caution
+	traceDataMemoryAccess(state, address, hostAddress, value, isWrite, isIO);
+}
+
+/*********************************************************************************
+ *	
+ *********************************************************************************/
+void MemoryAnalyzer::traceDataMemoryAccess(S2EExecutionState *state,
+								klee::ref<klee::Expr> &address,
+								klee::ref<klee::Expr> &hostAddress,
+								klee::ref<klee::Expr> &value
+								bool isWrite, bool isIO)
+{
+	bool isAddrCste = isa<klee::ConstantExpr>(address);
+	bool isValCste = isa<klee::ConstantExpr>(value);
+	bool isHostAddrCste = isa<klee::ConstantExpr>(hostAddress);
+
+	ExecutionTraceMemory e;
+	e.flags = 0;
+	e.pc = state->getPc();
+
+	uint64_t concreteAddress = 0xdeadbeef;
+	uint64_t concreteValue = 0xdeadbeef;
+
+/*
+	if(ConcolicMode) {
+		klee::ref<klee::ConstantExpr> 
+				ce = dyn_cast<klee::ConstantExpr>(state->concolics.evaluate(address)); 
+		concreteAddress = ce->getZExtValue();
+
+		ce = dyn_cast<klee::ConstantExpr>(state->concolics.evaluate(value));
+		concreteValue = ce->getZExtValue();
+	}
+*/
+
+	e.address = isAddrCste ? cast<klee::ConstantExpr>(address)->getZExtValue(64) : concreteAddress;
+	e.value = isValCste ? cast<klee::ConstantExpr>(value)->getZExtValue(64) : concreteValue;
+	e.size = klee::Expr::getMinBytesForWidth(value->getWidth());
+	e.flags = isWrite*EXECTRACE_MEM_WRITE | isIO*EXECTRACE_MEM_IO;
+	e.hostAddress = isHostAddrCste ? case<klee::ConstantExpr>(hostAddress)->getZExtValue(64) : 0xDEADBEEF;
+
+	if(!isAddrCste){
+		e.flags |= EXECTRACE_MEM_SYMBADDR;
+	}
+
+    if(!isValCste){
+		e.flags |= EXECTRACE_MEM_SYMBVAL;
+	}
+	
+    if(!isHostAddrCste){
+		e.flags |= EXECTRACE_MEM_SYMBHOSTADDR;
+	}
+	
+	unsigned strucSize = sizeof(e);
+	if(!(e.flags & EXECTRACE_MEM_HASHOSTADDR) && !(e.flags & EXECTRACE_MEM_OBJECTSTATE)){
+		strucSize -= (sizeof(e.hostAddress) + sizeof(e.concreteBuffer));	
+	}
+
+	m_tracer->writeData(state, &e, sizeof(e), TRACE_MEMORY);
 }
 
 /*********************************************************************************
--- qemu/s2e/Plugins/MemoryAnalyzer/MemoryAnalyzer.h
+++ qemu/s2e/Plugins/MemoryAnalyzer/MemoryAnalyzer.h
@@ -1,8 +1,10 @@
-#ifndef	_MEMORY_ANALYSIS_H
-#define _MEMORY_ANALYSIS_H
+#ifndef	_MEMORY_ANALYZER_H
+#define _MEMORY_ANALYZER_H
 
+#include <string>
 #include <s2e/Plugin.h>
-#include <s2e/Plugins/CorePlugin.h>
+#include <s2e/Plugins/ExecutionTracers/ExecutionTracer.h>
+#include <s2e/Plugins/ModuleExecutionDetector.h>
 #include <s2e/S2EExecutionState.h>
 
 namespace s2e {
@@ -11,6 +13,16 @@ namespace plugins {
 class MemoryAnalyzer: public Plugin
 {
 	S2E_PLUGIN
+private:
+	ExecutionTracer *m_tracer;
+	ModuleExecutionDetector *m_executionDetector
+
+	bool m_monitorModules;
+	bool m_memoryMonitor;
+	bool m_stackMonitor;
+	bool m_heapMonitor;
+
+	sigc::connection m_DataMemoryMonitor;
 
 public:
 	MemoryAnalyzer(S2E *s2e): Plugin(s2e) {	}
@@ -18,14 +30,30 @@ public:
 	sigc::signal<void, S2EExecutionState *, uint64_t> ;
 	
 	void initialize();
-	void onModuleLoad(S2EExecutionState *state, uint64_t pc);
+	void enableTracing();
+	void onModuleTransition(S2EExecutionState *state,
+							const ModuleDescriptor *prevModule,
+							const ModuleDescriptor *nextModule);
+	void onDataMemoryAccess(S2EExecutionState *state, 
+							klee::ref<klee::Expr> address,
+							klee::ref<klee::Expr> hostAddress,
+							klee::ref<klee::Expr> values,
+							bool isWrite, bool isIO);
+
 	void onMemoryWrite(S2EExecutionState *state, uint64_t pc);
+
+	void traceDataMemoryAccess(S2EExecutionState *state,
+								klee::ref<klee::Expr> &address,
+								klee::ref<klee::Expr> &hostAddress,
+								klee::ref<klee::Expr> &value
+								bool isWrite, bool isIO);
 
 };
 
 }	//namespace plugins
 }	//namespace s2e
 
+/*
 //Plugin State for per state plugin information.
 class MemoryAnalyzerState: public PluginState
 {
@@ -43,9 +71,7 @@ public:
         return new MemoryAnalyzerState(*this);
     }
 
-    void increment() { ++m_count; }
-    int get() { return m_count; }
-
 };
+*/
 #endif
 
